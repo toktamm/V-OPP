57a7fade15e3149e01e5b1dc8940b4f9450a053c
johncabang <johncabz@gmail.com>
2020-11-22 20:35:58 +0000

    Update post/posts feature, refactor, add mock data

--- client/src/components/Posts.js
+++ client/src/components/Posts.js
@@ -1,25 +1,28 @@
 import React from "react";
-import "./Posts.css";
 import Post from "./Post";
 
-function Posts() {
-  return (
-    <div className="posts">
-      <div className="posts__container">
-        <div className="posts__wrapper">
-          <ul className="posts__items">
+import "./Posts.css";
+
+function Posts(props) {
+  const { postData } = props;
+
+  const posts = postData
+    ? postData.map((postData, index) => {
+        return (
+          <div className="posts__container">
             <Post
-              src="images/img-9.jpg"
-              title="Earth Day Community Clean-Ups"
-              text="On Earth Day join hundreds of high school students to clean Toronto together! Choose a location that works best for you and sign-up with your friends.On Earth Day join hundreds of high school students to clean Toronto together! Choose a location that works best for you and sign-up with your friends."
-              detail="Click for more details"
-              path="/detailedPost"
+              key={index}
+              src={postData.src}
+              title={postData.title}
+              text={postData.text}
+              detail={postData.detail}
             />
-          </ul>
-        </div>
-      </div>
-    </div>
-  );
+          </div>
+        );
+      })
+    : "There is no post here";
+
+  return [posts];
 }
 
 export default Posts;
--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,14 +1,51 @@
+import React, { useState } from "react";
 import "../../App.css";
 import FindPost from "../FindPost";
 import HeroSection from "../HeroSection";
 import Posts from "../Posts";
 
+const initialPostData = [
+  {
+    src: "images/img-9.jpg",
+    title: "Earth Day Community Clean-Ups",
+    text:
+      "On Earth Day join hundreds of high school students to clean Toronto together! Choose a location that works best for you and sign-up with your friends.On Earth Day join hundreds of high school students to clean Toronto together! Choose a location that works best for you and sign-up with your friends.",
+    detail: "Click for more details",
+  },
+  {
+    src: "images/img-4.jpg",
+    title: "Wildlife Care",
+    text:
+      "Bunya nuts black-eyed pea prairie turnip leek lentil turnip greens parsnip. Sea lettuce lettuce water chestnut eggplant winter purslane fennel azuki bean earthnut pea ",
+    detail: "Click for more details",
+  },
+  {
+    src: "images/img-8.jpg",
+    title: "Volunteer Mentor",
+    text:
+      "Nori grape silver beet broccoli kombu beet greens fava bean potato quandong celery. Bunya nuts black-eyed pea prairie turnip leek lentil turnip. Sea lettuce lettuce water.",
+    detail: "Click for more details",
+  },
+];
+
 function Home() {
+  const [postData, setPostData] = useState(initialPostData);
+
+  const addNewPost = (text) => {
+    const newPost = {
+      src: "images/img-9.jpg",
+      title: "Earth Day Community Clean-Ups",
+      text: "",
+      detail: "Click for more details",
+    };
+
+    setPostData([newPost, ...postData]);
+  };
   return (
     <>
-      <HeroSection />
       <FindPost />
-      <Posts />
+      <HeroSection />
+      <Posts postData={postData} />
     </>
   );
 }
