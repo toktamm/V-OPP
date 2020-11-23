086fe1d62e8739e7f9b7b7e7c784788bc860bc84
johncabang <johncabz@gmail.com>
2020-11-23 21:35:25 +0000

    Update db

--- backend/db/schema/02_posts.sql
+++ backend/db/schema/02_posts.sql
@@ -3,7 +3,18 @@ DROP TABLE IF EXISTS posts CASCADE;
 CREATE TABLE posts(
     id SERIAL PRIMARY KEY NOT NULL,
     title VARCHAR(255) NOT NULL,
-    content TEXT,
+    organization VARCHAR(255) NOT NULL,
+    description TEXT,
+    thumbnail_photo_url VARCHAR(255) NOT NULL,
+    country VARCHAR(255) NOT NULL,
+    street VARCHAR(255) NOT NULL,
+    city VARCHAR(255) NOT NULL,
+    province VARCHAR(255) NOT NULL,
+    post_code VARCHAR(255) NOT NULL,
+    date_posted DATE NOT NULL,
+    start_date DATE NOT NULL,
+    requirements TEXT,
+    additional_info TEXT,
+    active BOOLEAN NOT NULL DEFAULT TRUE,
     user_id INTEGER REFERENCES users(id)
 );
-
--- backend/db/seeds/02_posts.sql
+++ backend/db/seeds/02_posts.sql
@@ -1,55 +1,20 @@
 INSERT INTO
-posts(title, content, user_id)
+posts(title, organization, description, thumbnail_photo_url, country, street, city, province, post_code, date_posted, start_date, requirements, additional_info, active, user_id)
 VALUES
     (
-        'Hey! Come back here! You big-a monkey!',
-        'In vel quam orci. Suspendisse potenti. Curabitur eget aliquam ex. Praesent ullamcorper scelerisque egestas. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi interdum viverra magna, ut gravida massa lacinia at. Fusce blandit quis lectus ac iaculis.',
-        1
+        'Earth Day Community Clean-ups', 'Earthday', 'On Earth Day join hundreds of high school students to clean Toronto together! Choose a location that works best for you and sign-up with your friends.On Earth Day join hundreds of high school students to clean Toronto together! Choose a location that works best for you and sign-up with your friends.', 'https://images.pexels.com/photos/346885/pexels-photo-346885.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260', 'Canada', '1 Yonge Street', 'Toronto', 'Ontario', 'M2D3K4', '2020-11-20', '2020-12-25', 'No previous experience necessary', 'N/A', true, 1
     ),
     (
-        'Nice of the princess to invite us over a picnic, eh Luigi?',
-        'In vel quam orci. Suspendisse potenti. Curabitur eget aliquam ex. Praesent ullamcorper scelerisque egestas. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi interdum viverra magna, ut gravida massa lacinia at. Fusce blandit quis lectus ac iaculis.',
-        1
+        'Volunteer Mentor', 'Education of Canada ', 'Be a leader in connecting students and creating our campus culture.Peer mentors act as advisors and offer support to incoming students. Peer mentors will offer support by providing access and referrals to appropriate resources regarding academic concerns or resources within Student Success and Engagement as needed. Peer Mentors will aid in the creation of a Faculty - Student connection, both inside the classroom and outside.', 'https://images.pexels.com/photos/935949/pexels-photo-935949.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260', 'Canada', '1265 Military Trail', 'Scarborough', 'Ontario', 'M1C1A4', '2020-11-2', '2021-01-09', 'Background check', 'N/A', true, 2
     ),
     (
-        'I wanna be a great plumber like my brother Mario.',
-        'In vel quam orci. Suspendisse potenti. Curabitur eget aliquam ex. Praesent ullamcorper scelerisque egestas. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi interdum viverra magna, ut gravida massa lacinia at. Fusce blandit quis lectus ac iaculis.',
-        2
+        'Crew Leader', 'Home 4U Ontario', 'Crew Leaders are an integral part of the Habitat mission. We look for volunteers who will be able to motivate, teach and lead a group of (potentially) inexperienced volunteers in a construction task for the day. If you have a combination of construction experience, team leadership skills and can contribute a full day’s work, this position may be for you.', 'https://images.pexels.com/photos/2098624/pexels-photo-2098624.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260', 'Canada', '755 Lawrence Ave E', 'North York', 'Ontario', 'M3C1P2', '2020-09-26', '2021-06-25', 'Background check', 'N/A', true, 3
     ),
     (
-        'Luigi, Luigi! Oh yeah! Oh yeah!',
-        'In vel quam orci. Suspendisse potenti. Curabitur eget aliquam ex. Praesent ullamcorper scelerisque egestas. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi interdum viverra magna, ut gravida massa lacinia at. Fusce blandit quis lectus ac iaculis.',
-        2
+        'Wildlife Videographer and Content Creator', 'Wildlife Society', 'Wildlife Society is pleased to be hiring a Wildlife Videographer and Content Creator Intern. We are a small organization that looking for someone who is passionate about documenting wildlife to share with the world.', 'https://images.pexels.com/photos/374719/pexels-photo-374719.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500', 'Canada', '220 Cranston Park Ave', 'Maple', 'Ontario', 'L6A2M4', '2020-10-10', '2021-01-20', 'N/A', 'N/A', true, 4
     ),
     (
-        'Did you see Toad and Chao and the sand?',
-        'In vel quam orci. Suspendisse potenti. Curabitur eget aliquam ex. Praesent ullamcorper scelerisque egestas. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi interdum viverra magna, ut gravida massa lacinia at. Fusce blandit quis lectus ac iaculis.',
-        3
-    ),
-    (
-        'Aw, brutal!',
-        'In vel quam orci. Suspendisse potenti. Curabitur eget aliquam ex. Praesent ullamcorper scelerisque egestas. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi interdum viverra magna, ut gravida massa lacinia at. Fusce blandit quis lectus ac iaculis.',
-        3
-    ),
-    (
-        'Whoo-hoo! Yippee!',
-        'In vel quam orci. Suspendisse potenti. Curabitur eget aliquam ex. Praesent ullamcorper scelerisque egestas. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi interdum viverra magna, ut gravida massa lacinia at. Fusce blandit quis lectus ac iaculis.',
-        4
-    ),
-    (
-        'Aww, sweet! I did it, yay!',
-        'In vel quam orci. Suspendisse potenti. Curabitur eget aliquam ex. Praesent ullamcorper scelerisque egestas. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi interdum viverra magna, ut gravida massa lacinia at. Fusce blandit quis lectus ac iaculis.',
-        4
-    ),
-    (
-        'Banana slamma!!',
-        'In vel quam orci. Suspendisse potenti. Curabitur eget aliquam ex. Praesent ullamcorper scelerisque egestas. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi interdum viverra magna, ut gravida massa lacinia at. Fusce blandit quis lectus ac iaculis.',
-        5
-    ),
-    (
-        'Help, Diddy! I ve fallen, and I can t get up! Can you pass me a banana, little buddy?',
-        'In vel quam orci. Suspendisse potenti. Curabitur eget aliquam ex. Praesent ullamcorper scelerisque egestas. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi interdum viverra magna, ut gravida massa lacinia at. Fusce blandit quis lectus ac iaculis.',
-        5
+        'Friendly Visiting Patients', 'Apple Healthcare', 'Be a friend to a patient, resident or client who may not have visitors on a regular basis by escorting them to various activities, taking them for walks or simply having a friendly chat. This position can be added to Meal Assistance. Shift times can accommodate your schedule.', 'https://images.pexels.com/photos/1642883/pexels-photo-1642883.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260', 'Canada', '3276 St Clair Ave E', 'Scarborough', 'Ontario', 'M1L1W1', '2020-09-20', '2020-12-25', 'Background check', 'N/A', true, 5
     );
 
     
