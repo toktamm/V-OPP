e906a28c561d78f5cc791126c5f5a25d665494f6
johncabang <johncabz@gmail.com>
2020-11-23 18:39:37 +0000

    Update link/volunteer/detail button

--- client/src/components/Post.js
+++ client/src/components/Post.js
@@ -4,30 +4,33 @@ import "./Post.css";
 import { Link } from "react-router-dom";
 
 function Post(props) {
-  console.log("props: -------" + props);
-  const { src, title, text, detail } = props;
+  const { src, title, text } = props;
 
   return (
     <>
-      {src && title && text && detail && (
+      {src && title && text && (
         <li className="posts__item">
-          <Link to="/detail" className="posts__item__link">
-            {/* <div className="posts"> */}
+          {/* <div className="posts"> */}
 
-            <div className="posts__wrapper">
-              <ul className="posts__items"></ul>
-              <figure className="posts__item__pic-wrap">
-                <img className="posts__item__img" src={src} />
-                <div className="posts__item__info">
-                  <h5 className="posts__item__title">{title}</h5>
-                  <h5 className="posts__item__text">{text}</h5>
-                  <h5 className="posts__item__detail">{detail}</h5>
-                </div>
-              </figure>
-            </div>
+          <div className="posts__wrapper">
+            <ul className="posts__items"></ul>
+            <figure className="posts__item__pic-wrap">
+              <img className="posts__item__img" src={src} alt="description" />
+              <div className="posts__item__info">
+                <h5 className="posts__item__title">{title}</h5>
+                <h5 className="posts__item__text">{text}</h5>
+                <Link
+                  to="/detailed"
+                  className="posts__item__link"
+                  style={{ textDecoration: "none" }}
+                >
+                  <button className="posts__volunteer__btn">volunteer</button>
+                </Link>
+              </div>
+            </figure>
+          </div>
 
-            {/* </div> */}
-          </Link>
+          {/* </div> */}
         </li>
       )}
     </>
